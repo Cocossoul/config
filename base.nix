5cd4af73430e9e4558fc68efb2429367a27e3028
Corentin PAPE <corentin.pape@epita.fr>
2021-10-09 20:18:44 +0000

    add nmtui

--- base.nix
+++ base.nix
@@ -13,6 +13,9 @@
   # networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  # nmtui FTW
+  network.networkmanager.enable = true;
+
   # Set your time zone.
   time = {
      timeZone = "Europe/Paris";
